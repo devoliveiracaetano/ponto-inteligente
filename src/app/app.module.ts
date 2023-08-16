44867b0d1694172b26ae1d296d4aac775570d661
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2023-08-16 19:41:11 -0300

    Capitulo 16 FINALIZADO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './autenticacao/login/login.module';
+import { LoginRoutingModule } from './autenticacao/login/login-routing.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,9 @@ import { LoginModule } from './autenticacao/login/login.module';
     BrowserAnimationsModule,
     MatButtonModule,
     LoginModule,
+    LoginRoutingModule,
+
+
     AppRoutingModule
   ],
   providers: [],
--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor (
+    private fb: FormBuilder,
+    private _snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
+    this.gerarForm()
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  logar() {
+    if (this.form.invalid) {
+      this._snackbar.open("Dados Inválidos", "Erro", { duration: 5000 });
+      return;
+    }
+    alert(JSON.stringify(this.form?.value));
+  }
+
 
 }
